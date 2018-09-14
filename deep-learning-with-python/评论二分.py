3abe990a9a5d695245d934129ed86f45f904d58c
huangzhouhong <778612084@qq.com>
2018-09-14 15:25:33 +0800

    code

--- deep-learning-with-python/评论二分.py
+++ deep-learning-with-python/评论二分.py
@@ -1,41 +1,36 @@
 
 #存在过拟合问题
+#binary_crossentropy适合二分问题
+#sigmoid将输出压缩到0-1
 
 from keras.datasets import imdb
 from keras import *
 import numpy as np
+def to_vector(datas):
+    vectors=np.zeros((len(datas),10000))
+    for i in range(len(datas)):
+        vectors[i,datas[i]]=1
+    return vectors
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+def load_data():
+    (train_data,train_label),(test_data,test_label)=imdb.load_data(num_words=10000)
+    return (to_vector(train_data),train_label.astype('float32')),(to_vector(test_data),test_label.astype('float32'))
 
-word_index=imdb.get_word_index()
-reverse_work_index=dict([(index,word) for (word,index) in word_index.items()])
-sentense=[reverse_work_index.get(i-3,'?') for i in train_data[0]]
-
-train_vectors=np.zeros((len(train_data),10000))
-for i in range(len(train_data)):
-    train_vectors[i,train_data[i]]=1
-train_result=train_labels.astype('float32')
-
-test_vectors=np.zeros((len(test_data),10000))
-for i in range(len(test_data)):
-    test_vectors[i,test_data[i]]=1
-test_result=test_labels.astype('float32')
+(train_data,train_label),(test_data,test_label)=load_data()
 
 network=models.Sequential()
-network.add(layers.Dense(32,activation='tanh',input_shape=(10000,)))
-network.add(layers.Dense(32,activation='tanh'))
+network.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
+network.add(layers.Dense(16,activation='relu'))
 network.add(layers.Dense(1,activation='sigmoid'))
-network.compile(optimizer=optimizers.rmsprop(),loss=losses.mse,metrics=['accuracy'])
-history = network.fit(train_vectors,train_result,batch_size=512,epochs=5,validation_data=(test_vectors,test_result))
 
-print(history)
+network.compile(optimizer=optimizers.rmsprop(),loss=losses.binary_crossentropy,metrics=['acc'])
+history = network.fit(train_data,train_label,batch_size=512,epochs=10,validation_data=(test_data,test_label))
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+x=range(1,len(acc)+1)
 
 import matplotlib.pyplot as plt
-
-y1=history.history['acc']
-x=range(1,len(y1)+1)
 plt.clf()
-plt.plot(x,y1,'bo',label='acc')
-plt.plot(x,history.history['val_acc'],'b',label='val_acc')
-plt.legend()
+plt.plot(x,acc,color='green')
+plt.plot(x,val_acc,'r+',color='red')
 plt.show()
